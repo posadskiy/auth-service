7f7127f7349f6051d608f5a614f71a77ebf93743
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2024-07-16 20:36:34 +0200

    Database + token refreshing

---
+++ auth-core/src/main/java/com/posadskiy/auth/core/storage/db/entity/RefreshTokenEntity.java
@@ -0,0 +1,80 @@
+package com.posadskiy.auth.core.storage.db.entity;
+
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.data.annotation.DateCreated;
+import io.micronaut.data.annotation.GeneratedValue;
+import io.micronaut.data.annotation.Id;
+import io.micronaut.data.annotation.MappedEntity;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.Instant;
+
+@Data
+@MappedEntity("refresh_token")
+public class RefreshTokenEntity {
+
+    @Id
+    @GeneratedValue
+    @NonNull
+    private Long id;
+
+    @NonNull
+    @NotBlank
+    private String username;
+
+    @NonNull
+    @NotBlank
+    private String refreshToken;
+
+    @NonNull
+    @NotNull
+    private Boolean revoked;
+
+    @DateCreated
+    @NonNull
+    @NotNull
+    private Instant dateCreated;
+
+    public @NonNull Long getId() {
+        return id;
+    }
+
+    public void setId(@NonNull Long id) {
+        this.id = id;
+    }
+
+    public @NonNull @NotBlank String getUsername() {
+        return username;
+    }
+
+    public void setUsername(@NonNull @NotBlank String username) {
+        this.username = username;
+    }
+
+    public @NonNull @NotBlank String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(@NonNull @NotBlank String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public @NonNull @NotNull Boolean getRevoked() {
+        return revoked;
+    }
+
+    public void setRevoked(@NonNull @NotNull Boolean revoked) {
+        this.revoked = revoked;
+    }
+
+    public @NonNull @NotNull Instant getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(@NonNull @NotNull Instant dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+}
