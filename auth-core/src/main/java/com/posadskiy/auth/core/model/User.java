d64cfec0c41d3fffa794d641028b0af583d24bce
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2024-10-14 22:43:38 +0200

    Remove user-related logic moved to separate service

--- auth-core/src/main/java/com/posadskiy/auth/core/model/User.java
+++
@@ -1,7 +0,0 @@
-package com.posadskiy.auth.core.model;
-
-import io.micronaut.core.annotation.Introspected;
-
-@Introspected
-public record User(Long id, String username, String email, String password) {
-}
