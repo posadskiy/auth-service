a012f9c212ab0493153bfa80f22dfd33c53de3e7
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2025-08-02 20:21:47 +0200

    Reduce password strength to avoid slowness issues

--- auth-service-core/src/main/java/com/posadskiy/auth/core/utils/PasswordMatcher.java
+++ auth-service-core/src/main/java/com/posadskiy/auth/core/utils/PasswordMatcher.java
@@ -5,7 +5,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Singleton
 public class PasswordMatcher {
-    public static final int PASSWORD_STRENGTH = 16;
+    public static final int PASSWORD_STRENGTH = 10;
     public static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(PASSWORD_STRENGTH);
 
     public static boolean match(String rawPassword, String encodedPassword) {
