58963a8e40cc4d3e8f44d5c73f7eff53910eb6b1
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2025-08-02 21:20:49 +0200

    Reduce password strength to 8

--- auth-service-core/src/main/java/com/posadskiy/auth/core/utils/PasswordEncoder.java
+++ auth-service-core/src/main/java/com/posadskiy/auth/core/utils/PasswordEncoder.java
@@ -5,7 +5,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Singleton
 public class PasswordEncoder {
-    public static final int PASSWORD_STRENGTH = 10;
+    public static final int PASSWORD_STRENGTH = 8;
     public static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(PASSWORD_STRENGTH);
 
     public static String encode(String rawPassword) {
